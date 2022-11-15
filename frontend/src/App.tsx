dea5db5bb0282996e5221753d54fb9111618280a
unknown <lucas.eumendes@gmail.com>
2022-11-15 10:47:39 -0300

    project created

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import NotificationButton from "./components/NotificationButton"
+import Header from "./components/header"
+import SalesCard from "./components/SalesCard"
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
